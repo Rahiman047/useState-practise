85b6d5f5f87628be1aaee2f59ca6e82e305e7a5b
Rahiman <abdurrahiman100@gmail.com>
2023-08-18 18:59:05 +0530

    state

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return "uncomment the below methods to test out the code";
+
+  // First Approach :- Anonymous arrow function method where instead of writing the function and defining the function using onclick
+  //we can follow as below
+  // return (
+  //   <div className="main">
+  //     <div>
+  //       <h1 className="main-text">Change the count Values by click</h1>
+  //       <>
+  //         <button type="button" onClick={() => setCount(count + 1)}>
+  //           +
+  //         </button>
+  //       </>
+  //       <p>{count}</p>
+  //       <>
+  //         <button type="button" onClick={() => setCount(count - 1)}>
+  //           -
+  //         </button>
+  //       </>
+  //     </div>
+  //     <div className="reset-button">
+  //       <button type="button" onClick={() => setCount(0)}>
+  //         Reset
+  //       </button>
+  //     </div>
+  //   </div>
+  // );
+
+  //second approach conventional approach more lines of code
+
+  // const increaseCount = () => {
+  //   setCount(count + 1);
+  // };
+
+  // const decreaseCount = () => {
+  //   if (count <= 0) {
+  //     return setCount(0);
+  //   }
+  //   return setCount(count - 1);
+  // };
+
+  // const resetCount = () => {
+  //   setCount(0);
+  //   if (count > 0) {
+  //     return alert("Count set to Zero");
+  //   }
+  // };
+
+  // return (
+  //   <div className="main">
+  //     <div>
+  //       <h1 className="main-text">Change the count Values by click</h1>
+  //       <>
+  //         <button type="button" onClick={increaseCount}>
+  //           +
+  //         </button>
+  //       </>
+  //       <p>{count}</p>
+  //       <>
+  //         <button type="button" onClick={decreaseCount}>
+  //           -
+  //         </button>
+  //       </>
+  //     </div>
+  //     <div className="reset-button">
+  //       <button type="button" onClick={resetCount}>
+  //         Reset
+  //       </button>
+  //     </div>
+  //   </div>
+  // );
 }
 
-export default App
+export default App;
